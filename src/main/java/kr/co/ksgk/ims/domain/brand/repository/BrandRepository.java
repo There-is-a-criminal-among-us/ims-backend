fbeeb6e4c910d01d12e02ace3d47866e3bcd44b5
Jinsoo Yoon <floreo1242@gmail.com>
2025-07-04 15:10:10 +0900

    Add EntityGraph to BrandRepository for eager loading of company associations

--- src/main/java/kr/co/ksgk/ims/domain/brand/repository/BrandRepository.java
+++ src/main/java/kr/co/ksgk/ims/domain/brand/repository/BrandRepository.java
@@ -1,9 +1,15 @@
 package kr.co.ksgk.ims.domain.brand.repository;
 
 import kr.co.ksgk.ims.domain.brand.entity.Brand;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
+
+    @EntityGraph(attributePaths = {"company"})
+    List<Brand> findAll();
 }
