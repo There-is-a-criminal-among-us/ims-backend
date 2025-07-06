232e04ab2a255d085898bc37f5be1cdaf3752634
Jinsoo Yoon <floreo1242@gmail.com>
2025-07-07 00:54:28 +0900

    Refactor CORS configuration to use dynamic allowed origins from application properties

--- src/main/java/kr/co/ksgk/ims/global/config/CorsConfig.java
+++ src/main/java/kr/co/ksgk/ims/global/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package kr.co.ksgk.ims.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,22 +9,19 @@ import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("#{'${app.cors.allowed-origins}'.split(',')}")
+    private List<String> allowedOrigins;
 
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-
-        // CORS 허용 URL
-        List<String> allowedOrigins = Collections.singletonList(
-                "http://localhost:4000"
-        );
         config.setAllowedOrigins(allowedOrigins);
         config.addAllowedHeader("*");
         config.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
