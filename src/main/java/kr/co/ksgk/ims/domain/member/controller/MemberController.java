eab70425d961e591fa531644038e2d00f834bd66
taehan0 <taehan1000@naver.com>
2025-07-14 15:56:42 +0900

    writing membercontroller.java

--- src/main/java/kr/co/ksgk/ims/domain/member/controller/MemberController.java
+++ src/main/java/kr/co/ksgk/ims/domain/member/controller/MemberController.java
@@ -1,5 +1,22 @@
 package kr.co.ksgk.ims.domain.member.controller;
 
+import  kr.co.ksgk.ims.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
 public class MemberController
 {
+    private final MemberService memberService;
+
+    @PostMapping
+    ResponseEntity
+
 }
