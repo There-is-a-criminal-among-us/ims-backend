9f4d4d943ba7dedcfd07a94beb2cf4fc9bc30be7
Taehan Kim <taehan1000@naver.com>
2025-07-25 18:50:04 +0900

    Update src/main/java/kr/co/ksgk/ims/domain/invoice/controller/InvoiceController.java
    Co-authored-by: Jinsoo Yoon <floreo1242@gmail.com>

--- src/main/java/kr/co/ksgk/ims/domain/invoice/controller/InvoiceController.java
+++ src/main/java/kr/co/ksgk/ims/domain/invoice/controller/InvoiceController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/invoice")
+@RequestMapping("/api/invoices")
 public class InvoiceController {
     private final InvoiceService invoiceService;
 
