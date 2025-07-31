3cd6c978086c4fb7fd069b30f2c034f5c15d328b
Jinsoo Yoon <floreo1242@gmail.com>
2025-07-31 18:00:54 +0900

    [feat] BrandResponse에 연관된 Company 정보 추가

--- src/main/java/kr/co/ksgk/ims/domain/brand/dto/response/BrandResponse.java
+++ src/main/java/kr/co/ksgk/ims/domain/brand/dto/response/BrandResponse.java
@@ -1,7 +1,7 @@
 package kr.co.ksgk.ims.domain.brand.dto.response;
 
-import jdk.jshell.Snippet;
 import kr.co.ksgk.ims.domain.brand.entity.Brand;
+import kr.co.ksgk.ims.domain.company.dto.response.CompanyResponse;
 import lombok.Builder;
 
 
@@ -9,13 +9,15 @@ import lombok.Builder;
 public record BrandResponse(
         long id,
         String name,
-        String note
+        String note,
+        CompanyResponse company
 ) {
     public static BrandResponse from(Brand brand) {
         return BrandResponse.builder()
                 .id(brand.getId())
                 .name(brand.getName())
                 .note(brand.getNote())
+                .company(CompanyResponse.from(brand.getCompany()))
                 .build();
     }
 }
