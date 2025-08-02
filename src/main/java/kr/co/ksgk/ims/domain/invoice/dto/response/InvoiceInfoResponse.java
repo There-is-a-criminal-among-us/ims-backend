c4a3baa631666383c8ac654c65c394e99797f2a4
Jinsoo Yoon <floreo1242@gmail.com>
2025-08-02 16:30:44 +0900

    Revert "[fix] 송장 조회 시 url 대신 keyName을 반환하도록 수정"
    This reverts commit fe0ba4e6d83c105d43c056478a524d940d6fd606.

--- src/main/java/kr/co/ksgk/ims/domain/invoice/dto/response/InvoiceInfoResponse.java
+++ src/main/java/kr/co/ksgk/ims/domain/invoice/dto/response/InvoiceInfoResponse.java
@@ -1,5 +1,6 @@
 package kr.co.ksgk.ims.domain.invoice.dto.response;
 
+import kr.co.ksgk.ims.domain.S3.service.S3Service;
 import kr.co.ksgk.ims.domain.invoice.entity.Invoice;
 import lombok.Builder;
 
@@ -13,19 +14,19 @@ public record InvoiceInfoResponse(
         String name,
         String phone,
         String number,
-        String invoiceKeyName,
-        String productKeyName,
+        String invoiceUrl,
+        String productUrl,
         LocalDateTime createdAt,
         List<InvoiceProductInfoResponse> products
 ) {
-    public static InvoiceInfoResponse from(Invoice invoice) {
+    public static InvoiceInfoResponse from(Invoice invoice, S3Service s3Service) {
         return InvoiceInfoResponse.builder()
                 .invoiceId(invoice.getId())
                 .name(invoice.getName())
                 .phone(invoice.getPhone())
                 .number(invoice.getNumber())
-                .invoiceKeyName(invoice.getInvoiceKeyName())
-                .productKeyName(invoice.getProductKeyName())
+                .invoiceUrl(s3Service.generateStaticUrl(invoice.getInvoiceKeyName()))
+                .productUrl(s3Service.generateStaticUrl(invoice.getProductKeyName()))
                 .createdAt(invoice.getCreatedAt())
                 .products(invoice.getInvoiceProducts().stream()
                         .map(InvoiceProductInfoResponse::from)
